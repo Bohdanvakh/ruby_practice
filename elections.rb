e17f3b8ebeabdc994360af3376fea517931589f8
Bohdan Vakh <Bohdanvah@gmail.com>
2024-01-03 19:03:13 +0200

    feat/check if somebody have got more than 50% of votes

--- elections.rb
+++ elections.rb
@@ -1,4 +1,5 @@
 require './elections_modules.rb'
+require './voting_modules.rb'
 require 'pry'
 require 'json'
 
@@ -61,7 +62,22 @@ def see_results
   result_number = elections.count
   puts "Elections results:"
 
+  @percentage_results = {}
+
   results.each do |key, value|
-    puts "#{key.to_s} --- #{value.to_f / result_number * 100}%"
+    candidate_results = "#{key.to_s} --- #{value.to_f / result_number * 100}%"
+
+    puts candidate_results
+
+    @percentage_results[key] = value.to_f / result_number * 100
+  end
+
+  elections_winner = @percentage_results.find { |key, value| value > 50 } # get a candidate with more than 50% of votes
+
+  if elections_winner
+    puts "The first round of elections is over"
+    puts "Candidate that have got more than 50% \n---> #{elections_winner[0]} with #{elections_winner[1]} votes"
+  else
+    puts "The first round of elections is over. The second round of elections is announced"
   end
 end
