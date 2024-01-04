123733a2705cb564137b750f823e72e9397e98a3
Bohdan Vakh <Bohdanvah@gmail.com>
2024-01-04 17:48:38 +0200

    feat/moved method from elections to module

--- elections_modules.rb
+++ elections_modules.rb
@@ -20,32 +20,26 @@ module ElectionsModule
       f.puts JSON.pretty_generate(obj)
     end
   end
+
+  def clean_json_file(file)
+    json = JSON.parse(File.read(file))
+    json.clear()
+
+    update_json(file, json)
+  end
+
+  def elections(candidates_file, people_file, elections_file)
+    people = JSON.parse(File.read(people_file))
+    @elections = JSON.parse(File.read(elections_file))
+
+    people.each do |person|
+      person_name = "#{person['first_name']} #{person['last_name']}"
+      candidates = JSON.parse(File.read(candidates_file))
+      result_hash = { person: person_name, result: candidates.sample }
+      @elections << result_hash
+    end
+
+    update_json('elections.json', @elections)
+    see_results
+  end
 end
-
-# module SaveToJson
-#   def save_to_json(obj, attributes)
-#     obj_data = JSON.parse(File.read(obj))
-#     obj_data << to_hash(attributes)
-
-#     File.open(obj, 'w') do |f|
-#       f.puts JSON.pretty_generate(obj_data)
-#     end
-#   end
-# end
-
-# module ToHash
-#   def to_hash(attributes)
-#     hash = {}
-#     attributes.each do |key, value|
-#       hash.store(key, value)
-#     end
-#   end
-# end
-
-# module UpdateJson
-#   def update_json(file_name, obj, method='w')
-#     File.open(file_name, method) do |f|
-#       f.puts JSON.pretty_generate(obj)
-#     end
-#   end
-# end
